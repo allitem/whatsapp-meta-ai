5790566f2d81c96280547235d9f4f4c736c70cb2
Rufio Dinoto(Musk) <244thanva@gmail.com>
2026-02-02 16:23:04 +0700

    Update repo_manager.py

--- app/repo_manager.py
+++ app/repo_manager.py
@@ -1,1 +1,6 @@
+import subprocess
 
+def git_push(msg="auto update by nika"):
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", msg])
+    subprocess.run(["git", "push"])
