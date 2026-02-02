c20556fe1aa939958a183b26ef71ee1c1cea1cf8
Rufio Dinoto(Musk) <244thanva@gmail.com>
2026-02-02 16:19:03 +0700

    Update workflow.py

--- app/workflow.py
+++ app/workflow.py
@@ -1,1 +1,19 @@
+import os
+import subprocess
 
+BASE = "runtime"
+
+def execute(cmd: str):
+    if "web" in cmd:
+        os.makedirs(f"{BASE}/web", exist_ok=True)
+        return "สร้างระบบเว็บเรียบร้อย"
+
+    if "game" in cmd:
+        os.makedirs(f"{BASE}/game", exist_ok=True)
+        return "สร้างระบบเกมเรียบร้อย"
+
+    if "database" in cmd:
+        os.makedirs(f"{BASE}/database", exist_ok=True)
+        return "สร้างระบบฐานข้อมูลเรียบร้อย"
+
+    return "ดำเนินการ workflow สำเร็จ"
